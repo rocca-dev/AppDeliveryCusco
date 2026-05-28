10bec72a732c4ab8c17379060075a3c684bf96d5
rocca-dev <215790@unsaac.edu.pe>
2026-05-28 00:38:54 -0500

    Delete AppDelivery directory

--- AppDelivery/modelos/__init__.py
+++
@@ -1,7 +0,0 @@
-from modelos.grafo import Grafo, Nodo, Arista
-from modelos.pedido import Pedido, Prioridad, EstadoPedido
-from modelos.repartidor import Repartidor, TipoVehiculo, CAPACIDADES_VEHICULO
-from modelos.cargador import (
-    inicializar_sistema, cargar_grafo, cargar_pedidos,
-    cargar_repartidores, guardar_pedidos,
-)
